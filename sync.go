7526af24afc7699639b954f8ffacfd0d3a51a3b1
Igor Ignatyev <iignatyev@izzy.agency>
2024-09-30 17:36:17 +0300

    propagate fixes

--- sync.go
+++ sync.go
@@ -7,6 +7,11 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/plumbing/storer"
+
 	"github.com/launchrctl/keyring"
 	"github.com/launchrctl/launchr/pkg/cli"
 	"github.com/launchrctl/launchr/pkg/log"
@@ -37,7 +42,7 @@ type SyncAction struct {
 // - Calls the propagate method to propagate resources' versions.
 // - Returns any error that occurs during the execution of the sync action.
 func (s *SyncAction) Execute(username, password, override, vaultpass string) error {
-	err := s.prepareArtifact(username, password, override)
+	hash, err := s.prepareArtifact(username, password, override)
 	if err != nil {
 		return err
 	}
@@ -53,7 +58,7 @@ func (s *SyncAction) Execute(username, password, override, vaultpass string) err
 		log.Info("- %s", file)
 	}
 
-	err = s.propagate(modifiedFiles, vaultpass)
+	err = s.propagate(modifiedFiles, vaultpass, hash)
 	if err != nil {
 		return err
 	}
@@ -68,19 +73,19 @@ func (s *SyncAction) Execute(username, password, override, vaultpass string) err
 	return nil
 }
 
-func (s *SyncAction) prepareArtifact(username, password, override string) error {
+func (s *SyncAction) prepareArtifact(username, password, override string) (*plumbing.Hash, error) {
 	repo, err := getRepo()
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	ci, errGet := s.keyring.GetForURL(artifactsRepositoryDomain)
 	if errGet != nil {
 		if errors.Is(errGet, keyring.ErrEmptyPass) {
-			return errGet
+			return nil, errGet
 		} else if !errors.Is(errGet, keyring.ErrNotFound) {
 			log.Debug("%s", errGet)
-			return errMalformedKeyring
+			return nil, errMalformedKeyring
 		}
 
 		ci.URL = artifactsRepositoryDomain
@@ -91,13 +96,13 @@ func (s *SyncAction) prepareArtifact(username, password, override string) error 
 			fmt.Printf("Please add login and password for URL - %s\n", ci.URL)
 			err = keyring.RequestCredentialsFromTty(&ci)
 			if err != nil {
-				return err
+				return nil, err
 			}
 		}
 
 		err = s.keyring.AddItem(ci)
 		if err != nil {
-			return err
+			return nil, err
 		}
 		s.saveKeyring = true
 	}
@@ -109,15 +114,73 @@ func (s *SyncAction) prepareArtifact(username, password, override string) error 
 		override: override,
 	}
 
-	err = storage.PrepareComparisonArtifact(s.comparisonDir)
+	hash, err := storage.PrepareComparisonArtifact(s.comparisonDir)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	return nil
+	return hash, nil
 }
 
-func (s *SyncAction) propagate(modifiedFiles []string, vaultpass string) error {
+type CommitInfo struct {
+	Hash         string
+	ChangedFiles []string
+}
+
+func testGitChanges(hash *plumbing.Hash) []*CommitInfo {
+	r, err := getRepo()
+	handleError(err)
+
+	// Get the HEAD reference
+	ref, err := r.git.Head()
+	handleError(err)
+
+	// Get an iterator to the commit history
+	cIter, err := r.git.Log(&git.LogOptions{From: ref.Hash()})
+	handleError(err)
+
+	var commits []*CommitInfo
+
+	err = cIter.ForEach(func(c *object.Commit) error {
+		if c.Hash.String() == hash.String() {
+			return storer.ErrStop
+		}
+
+		if strings.Contains(c.Message, "versions bump") {
+			fmt.Println(c.Hash)
+			stats, err := c.Stats()
+			if err != nil {
+				panic(err)
+			}
+
+			var files []string
+			for _, stat := range stats {
+				files = append(files, stat.Name)
+			}
+
+			ci := &CommitInfo{
+				Hash:         c.Hash.String(),
+				ChangedFiles: files,
+			}
+
+			commits = append(commits, ci)
+		}
+
+		return nil
+	})
+	handleError(err)
+
+	return commits
+}
+
+// handleError is a helper function to handle errors
+func handleError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (s *SyncAction) propagate(modifiedFiles []string, vaultpass string, hash *plumbing.Hash) error {
 	keyValueItem, errGet := s.keyring.GetForKey(vaultpassKey)
 	if errGet != nil {
 		if errors.Is(errGet, keyring.ErrEmptyPass) {
@@ -150,7 +213,71 @@ func (s *SyncAction) propagate(modifiedFiles []string, vaultpass string) error {
 		return err
 	}
 
-	updatedResources := inv.GetChangedResources(modifiedFiles)
+	// @todo
+	//    determine which resources are coming from artifact and copy their versions from artifact
+	//    propagate resources that were changes in commits.
+	//    ideally commit by commit.
+	allUpdatedResources := inv.GetChangedResources(modifiedFiles)
+
+	cli.Println("-----All modified resources-----")
+	for _, key := range allUpdatedResources.OrderedKeys() {
+		r, ok := allUpdatedResources.Get(key)
+		if !ok {
+			continue
+		}
+		cli.Println(r.GetName())
+	}
+
+	commits := testGitChanges(hash)
+	var commitsmodifiedFiles []string
+	for i := len(commits) - 1; i >= 0; i-- {
+		commit := commits[i]
+		commitsmodifiedFiles = append(commit.ChangedFiles)
+	}
+
+	cli.Println("-----Commits modified resources-----")
+	updatedResources := inv.GetChangedResources(commitsmodifiedFiles)
+	for _, key := range updatedResources.OrderedKeys() {
+		r, ok := updatedResources.Get(key)
+		if !ok {
+			continue
+		}
+
+		if _, okArt := allUpdatedResources.Get(key); okArt {
+			allUpdatedResources.Unset(key)
+		}
+
+		cli.Println(r.GetName())
+	}
+
+	cli.Println("-----Artifact modified resources-----")
+	for _, key := range allUpdatedResources.OrderedKeys() {
+		r, ok := allUpdatedResources.Get(key)
+		if !ok {
+			continue
+		}
+
+		cli.Println(r.GetName())
+		// @todo temporary
+		// set version from artifact to build dir
+
+		artifactResource := newResource(r.GetName(), s.comparisonDir)
+		if artifactResource.isValidResource() {
+			artifactVersion, err := artifactResource.GetVersion()
+			if err != nil {
+				return err
+			}
+			cli.Println("Artifact version of %s is %s", artifactResource.GetName(), artifactVersion)
+			if s.dryRun {
+				continue
+			}
+			err = r.UpdateVersion(artifactVersion)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
 	updatedVarResources, resourceVarsMap, err := inv.GetChangedVarsResources(modifiedFiles)
 	if err != nil {
 		return err
