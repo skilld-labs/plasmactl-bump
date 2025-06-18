d24750e77764bb5f3f549866cd39acdfb8adbf32
Igor Ignatyev <iignatyev@izzy.agency>
2025-06-18 16:02:37 +0300

    Move dependencies commands to bump plugin

--- plugin.go
+++ plugin.go
@@ -4,14 +4,19 @@ package plasmactlbump
 import (
 	"context"
 	_ "embed"
+	"fmt"
+	"os"
 
 	"github.com/launchrctl/keyring"
 	"github.com/launchrctl/launchr"
 	"github.com/launchrctl/launchr/pkg/action"
 )
 
-//go:embed action.yaml
-var actionYaml []byte
+//go:embed action.bump.yaml
+var actionBumpYaml []byte
+
+//go:embed action.dependencies.yaml
+var actionDependenciesYaml []byte
 
 func init() {
 	launchr.RegisterPlugin(&Plugin{})
@@ -39,8 +44,8 @@ func (p *Plugin) OnAppInit(app launchr.App) error {
 
 // DiscoverActions implements [launchr.ActionDiscoveryPlugin] interface.
 func (p *Plugin) DiscoverActions(_ context.Context) ([]*action.Action, error) {
-	a := action.NewFromYAML("bump", actionYaml)
-	a.SetRuntime(action.NewFnRuntime(func(_ context.Context, a *action.Action) error {
+	ba := action.NewFromYAML("bump", actionBumpYaml)
+	ba.SetRuntime(action.NewFnRuntime(func(_ context.Context, a *action.Action) error {
 		input := a.Input()
 		doSync := input.Opt("sync").(bool)
 		dryRun := input.Opt("dry-run").(bool)
@@ -88,7 +93,35 @@ func (p *Plugin) DiscoverActions(_ context.Context) ([]*action.Action, error) {
 
 		return nil
 	}))
-	return []*action.Action{a}, nil
+
+	da := action.NewFromYAML("dependencies", actionDependenciesYaml)
+	da.SetRuntime(action.NewFnRuntime(func(_ context.Context, a *action.Action) error {
+		log, _, _, term := getLogger(a)
+
+		input := a.Input()
+		source := input.Opt("source").(string)
+		if _, err := os.Stat(source); os.IsNotExist(err) {
+			term.Warning().Printfln("%s doesn't exist, fallback to current dir", source)
+			source = "."
+		} else {
+			term.Info().Printfln("Selected source is %s", source)
+		}
+
+		showPaths := input.Opt("mrn").(bool)
+		showTree := input.Opt("tree").(bool)
+		depth := int8(input.Opt("depth").(int)) //nolint:gosec
+		if depth == 0 {
+			return fmt.Errorf("depth value should not be zero")
+		}
+
+		target := input.Arg("target").(string)
+		dependencies := &dependenciesAction{}
+		dependencies.SetLogger(log)
+		dependencies.SetTerm(term)
+		return dependencies.run(target, source, !showPaths, showTree, depth)
+	}))
+
+	return []*action.Action{ba, da}, nil
 }
 
 func getLogger(a *action.Action) (*launchr.Logger, launchr.LogLevel, launchr.Streams, *launchr.Terminal) {
